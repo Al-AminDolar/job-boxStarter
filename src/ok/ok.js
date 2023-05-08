9c01c500ca1874d60804589f76c4b98b461a47b7
alamindolar <amin15-9898@diu.edu.bd>
2023-05-08 15:13:52 +0600

    done

--- src/ok/ok.js
+++ src/ok/ok.js
@@ -1,1 +1,16 @@
-here is nothing anything
+import React from "react";
+
+const ok = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fuga
+        consequuntur earum corrupti aspernatur aliquid pariatur exercitationem
+        nostrum iste neque dolores, error aliquam quasi, reiciendis adipisci
+        velit, maiores est quos?
+      </p>
+    </div>
+  );
+};
+
+export default ok;
