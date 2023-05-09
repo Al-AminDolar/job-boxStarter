4497d24fbd7d072c3ae1622201387e4f47699887
alamindolar <amin15-9898@diu.edu.bd>
2023-05-09 17:39:13 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import auth from "./firebase/firebase.config";
 
 function App() {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+      }
+    });
+  }, []);
   console.log(process.env);
   return (
     <>
