1fa4b86ebe412ab528631c98011094965cd2039a
alamindolar <alamin.dolar1@gmail.com>
2023-05-18 12:41:23 +0600

    ok

--- src/ONle/Onlyme.js
+++ src/ONle/Onlyme.js
@@ -6,6 +6,12 @@ const Onlyme = () => {
       <h1>ok</h1>
       <h3>Dollar</h3>
       <h4>mofig</h4>
+      klllkkas sasasa
+      <p>sss</p>
+      <hr />
+      <datagrid></datagrid>
+      <s>s</s>
+      <fieldset>ddd</fieldset>
     </div>
   );
 };
---
+++ src/ONle/ok.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ok = () => {
+  return <div>ssssssssssssssssssssss</div>;
+};
+
+export default ok;
