b5d9c61941fb462ede693b12ee180d9eec8921f8
alamindolar <alamin.dolar1@gmail.com>
2023-05-18 12:39:01 +0600

    contribution check

---
+++ src/ONle/Onlyme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Onlyme = () => {
+  return (
+    <div>
+      <h1>ok</h1>
+      <h3>Dollar</h3>
+      <h4>mofig</h4>
+    </div>
+  );
+};
+
+export default Onlyme;
